2985beb5bedd1aa923369eeda2855e0e9d156e98
Rajesh Vavadiya <rajeshv@shavik.ai>
2021-03-12 12:47:01 +0530

    dsds

--- lib/feacture/events_shows/presentation/bloc/event_show_bloc.dart
+++ lib/feacture/events_shows/presentation/bloc/event_show_bloc.dart
@@ -1,23 +1,29 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:comedy/feacture/events_shows/data/model/event_show_model.dart';
+import 'package:comedy/feacture/events_shows/domain/repository/event_show_repository.dart';
 import 'package:comedy/feacture/events_shows/domain/usecase/create_event_usecase.dart';
 import 'package:meta/meta.dart';
 
 part 'event_show_event.dart';
-
 part 'event_show_state.dart';
 
 class EventShowBloc extends Bloc<EventShowEvent, EventShowState> {
   EventShowBloc({
+    this.eventShowRepository,
     this.createEventUseCase,
   }) : super(EventShowInitial());
 
   final CreateEventUseCase createEventUseCase;
-
+  final EventShowRepository eventShowRepository;
   @override
   Stream<EventShowState> mapEventToState(EventShowEvent event) async* {
-
+    if (event is EventShowsEvents) {
+      final result = await eventShowRepository.createEvent();
+      if (result != null) {
+        print('Successs');
+      }
+    }
   }
-
 }
