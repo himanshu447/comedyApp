9e4da71affdbb6214174b47953718a67fb40d140
Rajesh Vavadiya <rajeshv@shavik.ai>
2021-03-13 16:10:36 +0530

    api work Continue

--- lib/feacture/events_shows/presentation/bloc/event_show_bloc.dart
+++ lib/feacture/events_shows/presentation/bloc/event_show_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:comedy/feacture/events_shows/data/model/event_show_model.dart';
 import 'package:comedy/feacture/events_shows/data/model/get_all_events_data.dart';
+import 'package:comedy/feacture/events_shows/domain/repository/event_show_repository.dart';
 import 'package:comedy/feacture/events_shows/domain/usecase/create_event_usecase.dart';
 import 'package:meta/meta.dart';
 
@@ -10,21 +11,20 @@ part 'event_show_event.dart';
 part 'event_show_state.dart';
 
 class EventShowBloc extends Bloc<EventShowEvent, EventShowState> {
-  CreateEventUseCase createEventUseCase;
-  AllEventsDatasUseCase allEventsDatasUseCase;
+  EventShowBloc({
+    this.eventShowRepository,
+    this.createEventUseCase,
+  }) : super(EventShowInitial());
 
-  EventShowBloc({this.allEventsDatasUseCase, this.createEventUseCase})
-      : super(EventShowInitial());
-
+  final CreateEventUseCase createEventUseCase;
+  final EventShowRepository eventShowRepository;
   @override
   Stream<EventShowState> mapEventToState(EventShowEvent event) async* {
-    if (event is EventAllDataGetEvents) {
-      yield LoadingBeginHomeState();
-      final result = await allEventsDatasUseCase(Alleventparam());
-      yield LoadingEndHomeState();
-
-      yield result.fold((error) => ErrorState(error.message),
-          (success) => EventallDataGet(allEventsData: success));
+    if (event is EventShowsEvents) {
+      final result = await eventShowRepository.createEvent();
+      if (result != null) {
+        print('Successs');
+      }
     }
   }
 }
