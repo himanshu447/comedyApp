62f9cffaee408dc3323b0d4e81392dcdf432277f
Himanshu <himanshu.gandhi447@gmail.com>
2021-06-15 23:50:40 +0530

    integrate real time admob

--- lib/utils/constant_util.dart
+++ lib/utils/constant_util.dart
@@ -15,9 +15,9 @@ class ConstantUtil {
 
   static String getBannerAdUnitId() {
     if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+      return "ca-app-pub-8426276217641603/2914809231";
     } else if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return "ca-app-pub-8426276217641603/2723237547";
     }
     return null;
   }
