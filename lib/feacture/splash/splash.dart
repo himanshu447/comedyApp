103f701ce0883a1e17dc5c2ff604e4e68ef8fbf7
Himanshu <himanshu.gandhi447@gmail.com>
2021-07-10 13:15:11 +0530

    fix

--- lib/feacture/splash/splash.dart
+++ lib/feacture/splash/splash.dart
@@ -10,7 +10,7 @@ class SplashView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //_navigateToNextScreen(context);
+    _navigateToNextScreen(context);
 
     return Scaffold(
       backgroundColor: Colors.white,
