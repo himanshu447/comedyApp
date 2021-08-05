dbcc902d15610a964845cc2a372b35bbe6a6e4f2
Himanshu <himanshu.gandhi447@gmail.com>
2021-08-05 11:00:01 +0530

    fix

--- lib/feacture/splash/splash.dart
+++ lib/feacture/splash/splash.dart
@@ -35,6 +35,7 @@ class SplashView extends StatelessWidget {
               ),
             ),
 
+
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
